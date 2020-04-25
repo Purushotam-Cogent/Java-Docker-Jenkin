9a40dbc6c0a669cbf6ea74d6608b6fcdf17d3ece
Purushotam Sinha <purushotam.sinha@cogentinfo.com>
2020-04-25 13:53:20 -0400

    Added new Java Class File

---
+++ target/classes/com/cogent/Application.java
@@ -0,0 +1,18 @@
+package com.cogent;
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Application.main()");
+		System.out.println("-----------------------------");
+		System.out.println("|                           |");
+		System.out.println("|                           |");
+		System.out.println("|       Spring Boot         |");
+		System.out.println("|                           |");
+		System.out.println("|                           |");
+		System.out.println("-----------------------------");
+		
+		
+		
+	}
+
+}
